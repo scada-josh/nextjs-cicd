0947eaadf5f8418763652363d44adc03191969ec
“Anusorn <scada.josh@gmail.com>
2025-06-16 19:06:34 +0700

    change Title DevJosh2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. (DevJosh)
+            Save and see your changes instantly. (DevJosh2)
           </li>
         </ol>
 
